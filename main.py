fff9bc92e394e1ab84630836b0a22d0062c5d34c
Verbalena <alen.verb@yandex.ru>
2024-11-24 17:10:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+rez1 = 2 * 2 + 2
+rez2 = 2 * (2 + 2)
+print(rez1)
+print(rez2)
+print(rez1 == rez2)
+
+# 4th program
+my_str = '123.456'
+rez3 = float(my_str) * 100
+rez4 = (rez3 // 10) % 10
+print(int(rez4))
+
+
+
+
+
